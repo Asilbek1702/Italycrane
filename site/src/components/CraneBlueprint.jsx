e8ef9062e95d880757abe5828b5c500a57a1354a
Asilbek1702 <asilbektashpulatov687@gmail.com>
2026-07-27 10:28:57 +0500

    first changing of site, 2

--- site/src/components/CraneBlueprint.jsx
+++ site/src/components/CraneBlueprint.jsx
@@ -2,20 +2,39 @@ import blueprint from "../assets/cranes/blueprint.png";
 
 export default function CraneBlueprint() {
   return (
-    <div style={{
-      position: "fixed", inset: 0, zIndex: 0, pointerEvents: "none",
-      display: "flex", alignItems: "center", justifyContent: "flex-end",
-      overflow: "hidden"
-    }}>
+    <div
+      style={{
+        position: "fixed",
+        inset: 0,
+        zIndex: 0,
+        pointerEvents: "none",
+        overflow: "hidden",
+
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       <img
         src={blueprint}
         alt=""
         style={{
-          height: "115%", width: "auto", maxWidth: "none",
-          objectFit: "cover", opacity: 0.5,
-          marginRight: "-6%",
-          maskImage: "linear-gradient(90deg, transparent 0%, #000 22%, #000 100%)",
-          WebkitMaskImage: "linear-gradient(90deg, transparent 0%, #000 22%, #000 100%)"
+          position: "absolute",
+
+          left: "50%",
+          top: "50%",
+          transform: "translate(-50%, -50%)",
+
+          width: "90vw",
+          height: "90vh",
+          objectFit: "contain",
+
+          opacity: 0.45,
+
+          maskImage:
+            "linear-gradient(to right, transparent 0%, black 18%, black 82%, transparent 100%)",
+          WebkitMaskImage:
+            "linear-gradient(to right, transparent 0%, black 18%, black 82%, transparent 100%)",
         }}
       />
     </div>
